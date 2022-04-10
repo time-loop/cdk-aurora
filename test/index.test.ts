893674630dc325582314e9b372f6900411129a63
Andrew Hammond <445764+ahammond@users.noreply.github.com>
2022-04-10 12:50:04 -0700

    refactor - strip out refs to core.Stack

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,36 +1,26 @@
-import { App, assertions, aws_kms, aws_ec2 } from 'aws-cdk-lib';
+import { App, assertions, aws_kms, aws_ec2, Stack, Duration } from 'aws-cdk-lib';
 import { Namer } from 'multi-convention-namer';
 
-import { core, aurora } from '../../src';
-
-// Minimum props required by @time-loop/cdk-library/core.StackProps
-const commonProps = {
-  businessUnit: core.BusinessUnit.PRODUCT,
-  clickUpEnvironment: core.ClickUpEnvironment.PRODUCTION,
-  clickUpRole: core.ClickUpRole.APP,
-  confidentiality: core.Confidentiality.PUBLIC,
-  namedEnv: core.Environment.usDev('us-west-2'),
-};
-const name = new Namer(['test']);
+import { Aurora } from '../src';
 
 describe('Aurora', () => {
-  describe('dev default', () => {
+  describe('default', () => {
     const app = new App();
-    const stack = new core.Stack(app, name, commonProps);
+    const stack = new Stack(app, 'test');
     const kmsKey = new aws_kms.Key(stack, 'Key');
-    new aurora.Aurora(stack, new Namer(['test']), { kmsKey });
+    const vpc = new aws_ec2.Vpc(stack, 'Vpc');
+    new Aurora(stack, new Namer(['test']), { kmsKey, vpc });
     const template = assertions.Template.fromStack(stack);
     it('creates resources', () => {
-      ['AWS::RDS::DBCluster', 'AWS::RDS::DBInstance', 'AWS::RDS::DBProxy'].forEach((r) =>
-        template.resourceCountIs(r, 1),
-      );
+      ['AWS::RDS::DBCluster', 'AWS::RDS::DBProxy'].forEach((r) => template.resourceCountIs(r, 1));
+      ['AWS::RDS::DBInstance'].forEach((r) => template.resourceCountIs(r, 2));
       ['AWS::SecretsManager::RotationSchedule', 'AWS::SecretsManager::Secret'].forEach((r) =>
         template.resourceCountIs(r, 3),
       );
     });
     it('retention', () => {
       template.hasResourceProperties('AWS::RDS::DBCluster', {
-        BackupRetentionPeriod: 2,
+        BackupRetentionPeriod: 1,
       });
     });
 
@@ -38,85 +28,92 @@ describe('Aurora', () => {
     //   template.hasOutput('ProxyEndpoint', {});
     // });
   });
-  describe('staging defaults', () => {
-    const app = new App();
-    const stack = new core.Stack(app, name, { ...commonProps, namedEnv: core.Environment.usStaging('us-west-2') });
-    const kmsKey = new aws_kms.Key(stack, 'Key');
-    new aurora.Aurora(stack, new Namer(['test']), { kmsKey });
-    const template = assertions.Template.fromStack(stack);
-    it('retention', () => {
-      template.hasResourceProperties('AWS::RDS::DBCluster', {
-        BackupRetentionPeriod: 7,
-      });
-    });
-  });
-  describe('production defaults', () => {
-    const app = new App();
-    const stack = new core.Stack(app, name, { ...commonProps, namedEnv: core.Environment.euProd('eu-central-1') });
-    const kmsKey = new aws_kms.Key(stack, 'Key');
-    new aurora.Aurora(stack, new Namer(['test']), { kmsKey });
-    const template = assertions.Template.fromStack(stack);
-    it('retention', () => {
-      template.hasResourceProperties('AWS::RDS::DBCluster', {
-        BackupRetentionPeriod: 30,
-      });
-    });
-  });
   describe('options', () => {
     it('bootstrap', () => {
       const app = new App();
-      const stack = new core.Stack(app, name, commonProps);
+      const stack = new Stack(app, 'test');
       const kmsKey = new aws_kms.Key(stack, 'Key');
-      new aurora.Aurora(stack, new Namer(['test']), { kmsKey, bootstrap: true });
+      const vpc = new aws_ec2.Vpc(stack, 'Vpc');
+      new Aurora(stack, new Namer(['test']), { kmsKey, vpc, bootstrap: true });
       const template = assertions.Template.fromStack(stack);
       ['AWS::SecretsManager::Secret'].forEach((r) => template.resourceCountIs(r, 3)); // Still have 3 users
       ['AWS::SecretsManager::RotationSchedule'].forEach((r) => template.resourceCountIs(r, 1)); // Only manager is rotated
     });
     it('defaultDatabaseName', () => {
       const app = new App();
-      const stack = new core.Stack(app, name, commonProps);
+      const stack = new Stack(app, 'test');
       const kmsKey = new aws_kms.Key(stack, 'Key');
-      new aurora.Aurora(stack, new Namer(['test']), { kmsKey, defaultDatabaseName: 'foo' });
+      const vpc = new aws_ec2.Vpc(stack, 'Vpc');
+      new Aurora(stack, new Namer(['test']), { kmsKey, vpc, defaultDatabaseName: 'foo' });
       const template = assertions.Template.fromStack(stack);
       template.hasResourceProperties('AWS::RDS::DBCluster', {
         DatabaseName: 'foo',
       });
     });
+    it('instances', () => {
+      const app = new App();
+      const stack = new Stack(app, 'test');
+      const kmsKey = new aws_kms.Key(stack, 'Key');
+      const vpc = new aws_ec2.Vpc(stack, 'Vpc');
+      new Aurora(stack, new Namer(['test']), {
+        instances: 12,
+        kmsKey,
+        vpc,
+      });
+      const template = assertions.Template.fromStack(stack);
+      template.resourceCountIs('AWS::RDS::DBInstance', 12);
+    });
     it('instanceType', () => {
       const app = new App();
-      const stack = new core.Stack(app, name, commonProps);
+      const stack = new Stack(app, 'test');
       const kmsKey = new aws_kms.Key(stack, 'Key');
-      new aurora.Aurora(stack, new Namer(['test']), {
-        kmsKey,
+      const vpc = new aws_ec2.Vpc(stack, 'Vpc');
+      new Aurora(stack, new Namer(['test']), {
         instanceType: aws_ec2.InstanceType.of(aws_ec2.InstanceClass.R6G, aws_ec2.InstanceSize.XLARGE24),
+        kmsKey,
+        vpc,
       });
       const template = assertions.Template.fromStack(stack);
       template.hasResourceProperties('AWS::RDS::DBInstance', {
         DBInstanceClass: 'db.r6g.24xlarge',
       });
     });
+    // I don't see how to test things that are outside the "Properties" block.
+    // it('removalPolicy', () => {
+    //   const app = new App();
+    //   const stack = new Stack(app, 'test');
+    //   const kmsKey = new aws_kms.Key(stack, 'Key');
+    //   const vpc = new aws_ec2.Vpc(stack, 'Vpc');
+    //   new Aurora(stack, new Namer(['test']), {
+    //     kmsKey,
+    //     removalPolicy: RemovalPolicy.SNAPSHOT,
+    //     vpc,
+    //   });
+    //   const template = assertions.Template.fromStack(stack);
+    //   template.hasResourceProperties('AWS::RDS::DBCluster', {
+    //     UpdateReplacePolicy: 'Snapshot',
+    //     DeletionPolicy: 'Snapshot',
+    //   });
+    // });
+    it('retention', () => {
+      const app = new App();
+      const stack = new Stack(app, 'test');
+      const kmsKey = new aws_kms.Key(stack, 'Key');
+      const vpc = new aws_ec2.Vpc(stack, 'Vpc');
+      new Aurora(stack, new Namer(['test']), { kmsKey, vpc, retention: Duration.days(30) });
+      const template = assertions.Template.fromStack(stack);
+      template.hasResourceProperties('AWS::RDS::DBCluster', {
+        BackupRetentionPeriod: 30,
+      });
+    });
     it('skipProxy', () => {
       const app = new App();
-      const stack = new core.Stack(app, name, commonProps);
+      const stack = new Stack(app, 'test');
       const kmsKey = new aws_kms.Key(stack, 'Key');
-      new aurora.Aurora(stack, new Namer(['test']), { kmsKey, skipProxy: true });
+      const vpc = new aws_ec2.Vpc(stack, 'Vpc');
+      new Aurora(stack, new Namer(['test']), { kmsKey, vpc, skipProxy: true });
       const template = assertions.Template.fromStack(stack);
       template.resourceCountIs('AWS::RDS::DBProxy', 0);
-    });
-    it('users', () => {
-      const app = new App();
-      const stack = new core.Stack(app, name, commonProps);
-      const kmsKey = new aws_kms.Key(stack, 'Key');
-      new aurora.Aurora(stack, new Namer(['test']), {
-        kmsKey,
-        users: ['one', 'two', 'three', 'four'].map((u) => {
-          return {
-            name: new Namer([u]),
-          };
-        }),
-      });
-      const template = assertions.Template.fromStack(stack);
-      template.resourceCountIs('AWS::SecretsManager::Secret', 5); // 4 users + 1 manager
     });
   });
 });
