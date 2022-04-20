bb2fb89d716f4b4f7a22640628212ce74d7c37da
Andrew Hammond <445764+ahammond@users.noreply.github.com>
2022-04-20 15:20:05 -0700

    WIP: tests

--- test/aurora.provisioner.test.ts
+++ test/aurora.provisioner.test.ts
@@ -1,7 +1,9 @@
-// import { createUser, conformPassword, grantPrivileges } from '../src/aurora.provisioner';
+import {} from '@aws-sdk/client-secretsmanager';
+
+import { createUser, conformPassword, grantPrivileges } from '../src/aurora.provisioner';
 
 describe('createUser', () => {
-  it.todo('skips if user already exists');
+  it('skips if user already exists', async () => {});
   it.todo('creates user if user does not exist');
   it.todo('logs on error');
 });
@@ -15,4 +17,11 @@ describe('grantPrivileges', () => {
   it.todo('grants for readers');
   it.todo('grants for writers');
   it.todo('logs on error');
+});
+
+describe('handler', () => {
+  it.todo('runs onCreate');
+  it.todo('runs onUpdate');
+  it.todo('runs onDelete');
+  it.todo('errors on unknown action');
 });
