d0851e6f70c9b276dcc970da8f8784edd0d8fd21
Andrew Hammond <445764+ahammond@users.noreply.github.com>
2022-04-20 11:16:50 -0700

    add skeleton tests

---
+++ test/aurora.provisioner.test.ts
@@ -0,0 +1,18 @@
+// import { createUser, conformPassword, grantPrivileges } from '../src/aurora.provisioner';
+
+describe('createUser', () => {
+  it.todo('skips if user already exists');
+  it.todo('creates user if user does not exist');
+  it.todo('logs on error');
+});
+
+describe('conformPassword', () => {
+  it.todo('conforms password');
+  it.todo('logs on error');
+});
+
+describe('grantPrivileges', () => {
+  it.todo('grants for readers');
+  it.todo('grants for writers');
+  it.todo('logs on error');
+});
