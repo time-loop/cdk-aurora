e5e70d0ca2b04477c1e7145f19688c3d6644d507
Andrew Hammond <445764+ahammond@users.noreply.github.com>
2023-03-16 12:50:54 -0700

    feat: db provisioner retries connect
    Fixes #35

--- test/aurora.provision-database.methods.test.ts
+++ test/aurora.provision-database.methods.test.ts
@@ -1,5 +1,5 @@
 import AWSMock from 'aws-sdk-mock';
-import { Client } from 'pg';
+import { Client, ClientConfig } from 'pg';
 import sinon from 'sinon';
 
 import { Methods } from '../src/aurora.provision-database';
@@ -36,6 +36,69 @@ describe('fetchSecret', () => {
     expect(r).toEqual(standardResult);
     expect(getSecretValueStub.callCount).toEqual(1);
     expect(getSecretValueStub.firstCall.args[0]).toEqual({ SecretId: 'fakeManagerSecretArn' });
+  });
+});
+
+describe('connect', () => {
+  const m = new Methods();
+  let connectStub: sinon.SinonStub;
+  let queryStub: sinon.SinonStub;
+  let setTimeoutStub: sinon.SinonStub;
+
+  beforeEach(() => {
+    connectStub = sinon.stub(Client.prototype, 'connect');
+    queryStub = sinon.stub(Client.prototype, 'query');
+  });
+
+  afterEach(() => {
+    connectStub.restore();
+    queryStub.restore();
+  });
+
+  const standardClientConfig: ClientConfig = {
+    host: 'managerHost',
+    port: 5432,
+    database: 'managerEngine',
+  };
+
+  it('succeeds', async () => {
+    connectStub.resolves();
+    queryStub.resolves({ rowCount: 1 }); // SELECT 1 should return 1 row.
+    await m.connect(standardClientConfig);
+    expect(connectStub.callCount).toEqual(1);
+    expect(queryStub.callCount).toEqual(1);
+    expect(setTimeoutStub.callCount).toEqual(0);
+  });
+
+  it('retries when failing to connect', async () => {
+    const maxRetries = 5;
+    const retryDelayMs = 1;
+    connectStub.rejects(new Error('whoopsie'));
+    await expect(m.connect(standardClientConfig, { maxRetries, retryDelayMs })).rejects.toThrowError('whoopsie');
+    expect(connectStub.callCount).toEqual(maxRetries + 1); // +1 because the first call is not a retry
+    expect(queryStub.callCount).toEqual(0);
+  });
+
+  it('retries when SELECT 1 fails', async () => {
+    const maxRetries = 5;
+    const retryDelayMs = 1;
+    connectStub.resolves();
+    queryStub.rejects(new Error('whoopsie'));
+    await expect(m.connect(standardClientConfig, { maxRetries, retryDelayMs })).rejects.toThrowError('whoopsie');
+    expect(connectStub.callCount).toEqual(maxRetries + 1);
+    expect(queryStub.callCount).toEqual(maxRetries + 1);
+  });
+
+  it('retries when SELECT 1 has rowcount != 1', async () => {
+    const maxRetries = 5;
+    const retryDelayMs = 1;
+    connectStub.resolves();
+    queryStub.resolves({ rowCount: 0 }); // SELECT 1 should return 1 row, but... ???
+    await expect(m.connect(standardClientConfig, { maxRetries, retryDelayMs })).rejects.toThrowError(
+      'expected 1 row, got 0',
+    );
+    expect(connectStub.callCount).toEqual(maxRetries + 1);
+    expect(queryStub.callCount).toEqual(maxRetries + 1);
   });
 });
 
